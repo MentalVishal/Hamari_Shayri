360ff529b1a191ac645092d9d52add6eec63c34f
vishal kumar <vishalkumar36565@gmail.com>
2023-10-24 11:15:14 +0530

    PrivateRoute

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -5,13 +5,21 @@ import { Register } from "../pages/Register";
 import { Home } from "../pages/Home";
 import { Landing } from "../pages/Landing";
 import { History } from "../pages/History";
+import { PrivateRoute } from "./PrivateRoute";
 
 export const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/home" element={<Home />} />
+      <Route
+        path="/home"
+        element={
+          <PrivateRoute>
+            <Home />
+          </PrivateRoute>
+        }
+      />
       <Route path="/" element={<Landing />} />
       <Route path="/history/:id" element={<History />} />
     </Routes>
