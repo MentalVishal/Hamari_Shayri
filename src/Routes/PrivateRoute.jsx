c504b829a5574802238d530c73f833865c5150bb
vishal kumar <vishalkumar36565@gmail.com>
2023-10-24 11:27:58 +0530

    bug fix

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -2,6 +2,6 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 export const PrivateRoute = ({ children }) => {
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem("Token");
   return token ? children : <Navigate to={"/login"} />;
 };
